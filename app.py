3c4eaa661478c8b42fb699b39a3eaa7f0bf948d8
Ronaldo de Lélis <delelis.locutor@gmail.com>
2020-01-05 17:24:18 -0300

    renomeei os arquivos jinja2 para .html

--- app.py
+++ app.py
@@ -7,26 +7,27 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return render_template('home.jinja2')
+    return render_template('home.html')
+
 
 @app.route('/auth', methods=['GET', 'POST'])
 def auth():
-    return render_template('auth.jinja2')
+    return render_template('auth.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.jinja2')
+    return render_template('register.html')
 
 
 @app.route('/new.jinja2', methods=['POST'])
 def new_post():
     return ''
 
+
 @app.route("/teste")
 def teste():
     return '<h1>Página de teste!!!</h1>'
-
 
 
 if __name__ == '__main__':
